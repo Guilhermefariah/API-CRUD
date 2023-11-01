b72ab1735d97d7c18b3e806747295e7e8a401380
Guilhermefariah <guilhermefaria321@gmail.com>
2023-11-01 14:14:24 -0300

    API-CRUD

--- api/db.js
+++ api/db.js
@@ -6,3 +6,11 @@ export const db = mysql.createConnection({
     password: "guidev123",
     database: "crud"
 })
+
+db.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  } else {
+    console.log("Conexão bem-sucedida com o banco de dados");
+  }
+});
