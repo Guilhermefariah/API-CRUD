e759a227609d3e428ef300a142ed76efb84eeed7
Guilhermefariah <guilhermefaria321@gmail.com>
2023-11-01 19:33:06 -0300

    API-CRUD

--- api/db.js
+++ api/db.js
@@ -7,10 +7,3 @@ export const db = mysql.createConnection({
     database: "crud"
 })
 
-db.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
-  } else {
-    console.log("Conexão bem-sucedida com o banco de dados");
-  }
-});
--- api/index.js
+++ api/index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
-
+const cors = require('cors'); 
 
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
 const app = express();
 
 
+app.use(cors());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
 
 const port = process.env.PORT || 8800;
 app.listen(port, () => {
