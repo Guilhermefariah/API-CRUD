ebc36d82b6de0b6f2185d732d8c60993bec85679
Guilhermefariah <guilhermefaria321@gmail.com>
2023-11-01 13:03:01 -0300

    API-CRUD

--- api/index.js
+++ api/index.js
@@ -3,15 +3,11 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
 
-
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
 const app = express();
 
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
 
 const port = process.env.PORT || 8800;
 app.listen(port, () => {
